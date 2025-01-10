5ea89319b68c2ef402c8f34835b7ad48f452b87f
Denis Ilyasov O <LazyZombieNya@gmail.com>
2025-01-10 17:58:01 +0500

    add parser

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import requests
 url = 'https://joyreactor.cc/'
 page = requests.get(url)
 print(page.status_code)
+filteredNews = []
+allNews = []
+
+soup = BeautifulSoup(page.text, "html.parser")
+print(soup)
